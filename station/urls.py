8cc1032931bc664678c5316aa04f2a3ae7c1e25d
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-07-15 07:07:07 +0300

    Add urls for each model

---
+++ station/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from station.views import (
+    CrewViewSet,
+    StationViewSet,
+    TrainViewSet,
+    OrderViewSet,
+    JourneyViewSet,
+    TicketViewSet,
+    RouteViewSet,
+    TrainTypeViewSet
+)
+
+router = routers.DefaultRouter()
+router.register("crew", CrewViewSet)
+router.register("stations", StationViewSet)
+router.register("trains", TrainViewSet)
+router.register("trains", TrainTypeViewSet)
+router.register("orders", OrderViewSet)
+router.register("journeys", JourneyViewSet)
+router.register("tickets", TicketViewSet)
+router.register("routes", RouteViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "station"
