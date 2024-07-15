e253c55ded6f79644521b53a213cae9f2697f9fd
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-07-15 07:10:12 +0300

    Add ViewSets for each model

--- station/views.py
+++ station/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from station.models import (
+    Crew,
+    Station,
+    Train,
+    Order,
+    Journey,
+    Ticket,
+    Route,
+    TrainType
+)
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+
+
+class StationViewSet(viewsets.ModelViewSet):
+    queryset = Station.objects.all()
+
+
+class TrainViewSet(viewsets.ModelViewSet):
+    queryset = Train.objects.all()
+
+
+class TrainTypeViewSet(viewsets.ModelViewSet):
+    queryset = TrainType.objects.all()
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+
+
+class JourneyViewSet(viewsets.ModelViewSet):
+    queryset = Journey.objects.all()
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
