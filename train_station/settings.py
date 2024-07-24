13ac8ae25c2f9e8b9fbea44df3e53c6afc6bd75f
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-07-24 18:45:32 +0300

    Increase token lifetime

--- train_station/settings.py
+++ train_station/settings.py
@@ -153,7 +153,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=2431245),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
