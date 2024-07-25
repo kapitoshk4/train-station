d1e982427eacffa35c4300a7fde761082d7d0524
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-07-25 20:11:11 +0300

    Add get_serializer_class, get_queryset & pagination_class to OrderViewSet

--- station/views.py
+++ station/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count, F
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from station.models import (
     Crew,
@@ -25,7 +26,7 @@ from station.serializers import (
     RouteListSerializer,
     RouteRetrieveSerializer,
     TrainListSerializer,
-    TrainRetrieveSerializer
+    TrainRetrieveSerializer, OrderListSerializer
 )
 
 
@@ -41,20 +42,22 @@ class StationViewSet(viewsets.ModelViewSet):
 
 class TrainViewSet(viewsets.ModelViewSet):
     queryset = Train.objects.all()
+    serializer_class = TrainSerializer
 
     def get_serializer_class(self):
+        serializer_class = self.serializer_class
         if self.action == "list":
-            return TrainListSerializer
+            serializer_class = TrainListSerializer
         if self.action == "retrieve":
-            return TrainRetrieveSerializer
+            serializer_class = TrainRetrieveSerializer
 
-        return TrainSerializer
+        return serializer_class
 
     def get_queryset(self):
         queryset = self.queryset
 
         if self.action in ("list", "retrieve"):
-            return queryset.select_related("train_type")
+            queryset = queryset.select_related("train_type")
 
         return queryset
 
@@ -64,35 +67,55 @@ class TrainTypeViewSet(viewsets.ModelViewSet):
     serializer_class = TrainTypeSerializer
 
 
+class OrderSetPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = "page_size"
+    max_page_size = 20
+
+
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    pagination_class = OrderSetPagination
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user)
+        queryset = self.queryset.filter(user=self.request.user)
+
+        if self.action == "list":
+            queryset = queryset.prefetch_related("tickets__journey__train", "tickets__journey__crew")
+
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+    def get_serializer_class(self):
+        serializer_class = self.serializer_class
+        if self.action == "list":
+            serializer_class = OrderListSerializer
+
+        return serializer_class
+
 
 class JourneyViewSet(viewsets.ModelViewSet):
     queryset = Journey.objects.all()
+    serializer_class = JourneySerializer
 
     @staticmethod
     def _params_to_int(query_string):
         return [int(str_id) for str_id in query_string.split(",")]
 
     def get_serializer_class(self):
+        serializer_class = self.serializer_class
         if self.action == "list":
-            return JourneyListSerializer
+            serializer_class = JourneyListSerializer
         if self.action == "retrieve":
-            return JourneyRetrieveSerializer
+            serializer_class = JourneyRetrieveSerializer
 
-        return JourneySerializer
+        return serializer_class
 
     def get_queryset(self):
         queryset = self.queryset
-
         crews = self.request.query_params.get("crews")
 
         if crews:
@@ -103,7 +126,7 @@ class JourneyViewSet(viewsets.ModelViewSet):
             queryset = (
                 queryset
                 .select_related("route", "train")
-                .prefetch_related("crew")
+                .prefetch_related("crew", "route__destination", "route__source")
                 .annotate(
                     tickets_available=(
                             F("train__places_in_cargo")
@@ -113,10 +136,10 @@ class JourneyViewSet(viewsets.ModelViewSet):
                 )
             ).order_by("id")
         if self.action == "retrieve":
-            return (
+            queryset = (
                 queryset
                 .select_related("route", "train")
-                .prefetch_related("crew")
+                .prefetch_related("crew", "route__destination", "route__source")
             )
 
         return queryset.distinct()
@@ -129,19 +152,21 @@ class TicketViewSet(viewsets.ModelViewSet):
 
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all()
+    serializer_class = RouteSerializer
 
     def get_serializer_class(self):
+        serializer_class = self.serializer_class
         if self.action == "list":
-            return RouteListSerializer
+            serializer_class = RouteListSerializer
         if self.action == "retrieve":
-            return RouteRetrieveSerializer
+            serializer_class = RouteRetrieveSerializer
 
-        return RouteSerializer
+        return serializer_class
 
     def get_queryset(self):
         queryset = self.queryset
         if self.action in ("list", "retrieve"):
-            return (
+            queryset = (
                 queryset
                 .select_related("source", "destination")
             )
