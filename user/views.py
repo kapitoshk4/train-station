03ad6d5c9a81b7378854bbd41a25fa9b07a0d9b4
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-07-26 19:41:30 +0300

    Add LoginUserView

--- user/views.py
+++ user/views.py
@@ -1,13 +1,20 @@
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.settings import api_settings
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = ()
+
+
+class LoginUserView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = AuthTokenSerializer
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
