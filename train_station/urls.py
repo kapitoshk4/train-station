9404a39b223677aff8af3f147a6f8b24734deb6f
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-09-07 23:49:02 +0300

    Add v1 to urls in urls.py

--- train_station/urls.py
+++ train_station/urls.py
@@ -26,16 +26,16 @@ from train_station import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/station/", include("station.urls"), name="station"),
-    path("api/user/", include("user.urls"), name="user"),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/v1/station/", include("station.urls"), name="station"),
+    path("api/v1/user/", include("user.urls"), name="user"),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/doc/swagger/",
+        "api/v1/doc/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui"
     ),
     path(
-        "api/doc/redoc/",
+        "api/v1/doc/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc"
     ),
